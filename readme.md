e1091a16e54bed4211b0ed920d85d5ae001d4511
Leonardo Quadros Fragozo <leo.fragozo.dev@gmail.com>
2023-03-30 14:42:43 -0300

    Finish the first version of the etch a sketch

---
+++ readme.md
@@ -0,0 +1,1 @@
+"This is The Odin Project's 'Etch a Sketch' Foundation Section Project."
--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const container = document.getElementById("my-container");
-const newDiv = document.createElement("div");
-newDiv.classList.add("my-class");
-newDiv.setAttribute("id", "my-div");
-newDiv.textContent = "This is a div test";
-newDiv.style.width = '120px';
-newDiv.style.height = '120px';
-newDiv.style.marginTop = '250px';
-newDiv.style.marginLeft = '720px';
-newDiv.style.backgroundColor = 'rgb(255, 255, 255)';
 
-container.appendChild(newDiv);
+container.addEventListener("click", (e) => {
+  const x = e.clientX - container.getBoundingClientRect().left;
+  const y = e.clientY - container.getBoundingClientRect().top;
+  createDiv(x, y);
+});
 
-function changeColor () {
-  r = Math.floor(Math.random() * 256),
-  g = Math.floor(Math.random() * 256),
-  b = Math.floor(Math.random() * 256);
+function createDiv(x, y) {
+  const newDiv = document.createElement("div");
 
-  newDiv.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-};
+  newDiv.style.width = '150px';
+  newDiv.style.height = '150px';
+  newDiv.style.position = 'absolute';
+  newDiv.style.left = `${x}px`;
+  newDiv.style.top = `${y}px`;
+  newDiv.style.backgroundColor = changeColor();
+  newDiv.addEventListener("click", function() {
+      this.style.backgroundColor = changeColor();
+  });
+  container.appendChild(newDiv);
+}
 
-newDiv.addEventListener("click", changeColor);
+function changeColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
